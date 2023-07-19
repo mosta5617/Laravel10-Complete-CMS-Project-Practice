9056f97a9b6a0b337b65e8670f69264831a7eafa
Mostafijur-Rahman <mosta5617@gmail.com>
2023-07-19 09:08:07 +0600

    blog category route created

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -35,7 +35,16 @@
         
                     </ul>
                 </li>
-
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="ri-account-circle-line"></i>
+                        <span>Blog Category</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="{{ route('all.blog.category') }}">All Blog Category</a></li>
+                        <li><a href="auth-login.html">Add Blog Category</a></li>
+                    </ul>
+                </li>
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
                         <i class="ri-mail-send-line"></i>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Home\AboutController;
+use App\Http\Controllers\Home\BlogCategoryController;
 use App\Http\Controllers\Home\HomeSliderController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -62,6 +63,13 @@ Route::middleware('auth')->group(function () {
         Route::post('/update/multi/image', 'UpdateMultiImage')->name('update.multi.image');
         Route::get('/delete/multi/image/{id}', 'DeleteMultiImage')->name('delete.multi.image'); 
     });
+
+     // Blog Category All Routes 
+    Route::controller(BlogCategoryController::class)->group(function () {
+    Route::get('/all/blog/category', 'AllBlogCategory')->name('all.blog.category');
+
+
+});
 
 
 // });
