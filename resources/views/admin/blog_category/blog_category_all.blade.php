bc1b3c236c1a32c0fb2f80875b6213b50d8296dd
Mostafijur-Rahman <mosta5617@gmail.com>
2023-07-19 09:15:30 +0600

    blog category crud setup

--- resources/views/admin/blog_category/blog_category_all.blade.php
+++ resources/views/admin/blog_category/blog_category_all.blade.php
@@ -6,7 +6,7 @@
                     <div class="container-fluid">
 
                         <!-- start page title -->
-                        <div class="row">
+                        <div class="row ">
                             <div class="col-12">
                                 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
                                     <h4 class="mb-sm-0">Blog Category All</h4>
@@ -23,10 +23,10 @@
             <div class="card">
                 <div class="card-body">
 
-                    <h4 class="card-title">Blog Category All Data </h4>
+                   <center> <h4 class="card-title ">Blog Category All Data </h4></center>
 
 
-                    <table id="datatable" class="table table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                    <table id="datatable" class="table table-bordered dt-responsive nowrap " style="border-collapse: collapse; border-spacing: 0; width: 100%;">
                         <thead>
                         <tr>
                             <th>Sl</th>
@@ -44,9 +44,9 @@
                             <td> {{ $item->blog_category }} </td> 
 
                             <td>
-   <a href="" class="btn btn-info sm" title="Edit Data">  <i class="fas fa-edit"></i> </a>
+   <a href="{{ route('edit.blog.category',$item->id) }}" class="btn btn-info sm" title="Edit Data">  <i class="fas fa-edit"></i> </a>
 
-     <a href="" class="btn btn-danger sm" title="Delete Data" id="delete">  <i class="fas fa-trash-alt"></i> </a>
+     <a href="{{ route('delete.blog.category',$item->id) }}" class="btn btn-danger sm" title="Delete Data" id="delete">  <i class="fas fa-trash-alt"></i> </a>
 
                             </td>
 
--- routes/web.php
+++ routes/web.php
@@ -67,6 +67,11 @@ Route::middleware('auth')->group(function () {
      // Blog Category All Routes 
     Route::controller(BlogCategoryController::class)->group(function () {
     Route::get('/all/blog/category', 'AllBlogCategory')->name('all.blog.category');
+    Route::get('/add/blog/category', 'AddBlogCategory')->name('add.blog.category');
+    Route::post('/store/blog/category', 'StoreBlogCategory')->name('store.blog.category');
+    Route::get('/edit/blog/category/{id}', 'EditBlogCategory')->name('edit.blog.category');
+    Route::post('/update/blog/category/{id}', 'UpdateBlogCategory')->name('update.blog.category');
+    Route::get('/delete/blog/category/{id}', 'DeleteBlogCategory')->name('delete.blog.category');
 
 
 });
